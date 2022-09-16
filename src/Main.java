5819c107782d3f74c45ea2fad79e9b5409cad36c
Your Name <dat054760@gmail.com>
2022-09-16 14:57:43 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+import ra.entity.Product;
+
+import java.util.Scanner;
+
+public class Main {
+    static int index = -1;
+    //khoi tao mang toan cuc
+    static Product[] arrProduct = new Product[1000];
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        //xay dung menu
+        do {
+            System.out.println("*******MENU*******");
+            System.out.println("1.nhap thong tin cac san pham");
+            System.out.println("2.tinh gia ban cac san pham");
+            System.out.println("3.hien thi thong tin cac san pham");
+            System.out.println("4.tim kiem cac san pham");
+            System.out.println("5.sap xep cac sam pham theo gia tang dan");
+            System.out.println("6.thoat");
+            int choice = Integer.parseInt(sc.nextLine());
+            switch (choice) {
+                case 1:
+                    Main.inputListProduct(sc);
+                    break;
+                case 2:
+                    Main.calListExportPrice();
+                    break;
+                case 3:
+                    Main.displayListProduct();
+                    break;
+                case 4:
+                    Main.searchProductByName(sc);
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.exit(0);
+                default:
+                    System.out.println("nhap tu 1 den 6");
+            }
+        }while (true);
+
+    }
+    public static void inputListProduct(Scanner sc){
+        System.out.println("nhap so luong san pham can nhap thong tin: ");
+        int cnt = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < cnt; i++) {
+            // khoi tao doi tuong san pham de nhap thong tin
+            Product pro = new Product();
+            pro.inputData(sc);
+            // them san pham vao danh sach san pham
+            index++;
+            arrProduct[index] = pro;
+        }
+    }
+    public static void displayListProduct(){
+        for (int i = 0; i <= index; i++) {
+            arrProduct[i].displayData();
+        }
+    }
+    public static void calListExportPrice(){
+        //Duyen tung phan tu
+        for (int i = 0; i <= index; i++) {
+            //Đối tượng product
+            arrProduct[i].calExportPrice();
+        }
+    }
+    public static void searchProductByName(Scanner sc){
+        //Dung co kiem tra trong danh sach có tên cần tìm hay không
+        boolean checkExist = false;
+        //false là không tồn tại, true là có tồn tại
+        //1.nhap ten san pham can tim
+        System.out.println("nhap ten san pham can tim");
+        String strSearch = sc.nextLine();
+        //Duyet tung san pham
+        for (int i = 0; i <= index; i++){
+            //3.So sanh ten san pham can tìm với ten của từng sản phẩm trong danh sách
+            if (arrProduct[i].getProductName().equals(strSearch)){
+                Main.displayListProduct();
+                checkExist = true;
+            }
+        }
+        if (!checkExist) {
+            System.out.println("không tồn tại sản phẩm nào có tên trong danh sách");
+        }
+    }
+
+}
---
+++ src/ra/entity/Product.java
@@ -0,0 +1,91 @@
+package ra.entity;
+
+import java.util.Scanner;
+
+public class Product {
+    private  String productId;
+    private  String productName;
+    private float importPrice;
+    private float exportPrice;
+    private int quantity;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(String productId, String productName, float importPrice, float exportPrice, int quantity, String description) {
+        this.productId = productId;
+        this.productName = productName;
+        this.importPrice = importPrice;
+        this.exportPrice = exportPrice;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getImportPrice() {
+        return importPrice;
+    }
+
+    public void setImportPrice(float importPrice) {
+        this.importPrice = importPrice;
+    }
+
+    public float getExportPrice() {
+        return exportPrice;
+    }
+
+    public void setExportPrice(float exportPrice) {
+        this.exportPrice = exportPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void inputData(Scanner sc){
+        System.out.println("Nhap ma san pham" );
+        this.productId = sc.nextLine();
+        System.out.println("nhap ten san pham");
+        this.productName = sc.nextLine();
+        System.out.println("nhap gia san pham" );
+        this.importPrice = Float.parseFloat(sc.nextLine());
+        System.out.println("nhap so luong san pham");
+        this.quantity = Integer.parseInt(sc.nextLine());
+        System.out.println("nhap mo ta san pham");
+        this.description = sc.nextLine();
+    }
+    public void calExportPrice(){
+        this.exportPrice = this.importPrice*1.2F;
+    }
+    public  void displayData(){
+        System.out.printf("Ma SP: %s- Ten Sp: %s - Gia nhap: %f \n",this.productId,this.productName,this.importPrice);
+        System.out.printf("Gia ban: %.2f- Số Lượng: %d - Mô tả: %s \n",this.exportPrice,this.quantity,this.description);
+    }
+}
